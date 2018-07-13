106c15a21368598a7835067e2d1c4a6c5b15313d
zeynel.demirci <zeynel.demirci@gmail.com>
2018-07-13 18:18:26 +0100

    Single greeting fix

---
+++ src/main/java/com/example/labclustering/OldAproach.java
@@ -0,0 +1,21 @@
+package com.example.labclustering;
+
+import com.hazelcast.config.ClasspathXmlConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
+
+import java.io.FileNotFoundException;
+
+public class App {
+  public static void main(String[] args) {
+    Config config;
+    try {
+      config = new XmlConfigBuilder("./config/cluster-config.xml").build();
+    } catch (FileNotFoundException e) {
+      //ignore set defaults from classpath resource file
+      config = new ClasspathXmlConfig("cluster-config.xml");
+    }
+
+    new WrapperClusterNode(config);
+  }
+}
